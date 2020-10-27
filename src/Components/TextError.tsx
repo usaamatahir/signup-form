f6bfc7d5b26a83c8f1dedc5ed205b8c419f75c72
usaamatahir <usamatahir8330@gmail.com>
2020-10-27 03:40:12 -0700

    Update TextError.tsx

--- src/Components/TextError.tsx
+++ src/Components/TextError.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const TextError = (props: any) => {
-    return <div className="error">{props.children}</div>;
-}
+  return <div className="error">{props.children}</div>;
+};
 
-export default TextError
+export default TextError;
